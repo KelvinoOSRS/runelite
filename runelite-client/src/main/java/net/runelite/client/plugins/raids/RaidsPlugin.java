bf610cf15cfc606098b9e3aefd43a87dd9df65c4
Kamiel <k.vanflierenburg@outlook.com>
2018-04-30 02:49:16 +0200

    Add toggle for keeping the scout overlay active during the raid

--- runelite-client/src/main/java/net/runelite/client/plugins/raids/RaidsPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/raids/RaidsPlugin.java
@@ -262,8 +262,11 @@ public class RaidsPlugin extends Plugin
 
 		if (client.getSetting(Setting.IN_RAID_PARTY) == -1)
 		{
-			overlay.setScoutOverlayShown(false);
-			raid = null;
+			if (!inRaidChambers || !config.scoutOverlayDuringRaid())
+			{
+				overlay.setScoutOverlayShown(false);
+				raid = null;
+			}
 		}
 	}
 
